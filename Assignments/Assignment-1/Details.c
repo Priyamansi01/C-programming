c60f431a81fe4a6e8c46ee2b187519431dcbe243
Priya_Manasi_Kullu <priyakullu00@gmail.com>
2021-07-04 21:10:41 +0530

    Remove

---
+++ Assignments/Assignment-1/Details.c
@@ -0,0 +1,7 @@
+//WAp to display your name address, college name, phone no.
+#include"stdio.h"
+main()
+{
+    printf("\n Name- Priya Manasi Kullu\n Address- Laing\n College Name- RITE\n Phone no.- 9876543210\n ");
+    return 0;
+}
