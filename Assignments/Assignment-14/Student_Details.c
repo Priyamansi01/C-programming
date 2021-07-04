977b9d8f3fffa91b2cb8feeed2c0c8c668047c05
Priya_Manasi_Kullu <priyakullu00@gmail.com>
2021-07-04 20:25:51 +0530

    Add new folder

--- Assignments/Assignment-14/Student_Details.c
+++ Assignments/Assignment-14/Student_Details.c
@@ -2,27 +2,28 @@
 #include"stdio.h"
 main()
 {
-    char n[50], r[15], e[60], p[10];
+    char n1[10],n2[10],n3[10],r[15],p[10];
+    char email[35];
 
-    printf("\n\n\t\t\tEnter Student Details:");
-    printf("\t\t\t------------------------");
+    printf("\n\n\t\t\tEnter Student Details:\n");
+    printf("\t\t\t----------------------");
 
     printf("\n\tEnter Student Name \t:");
-    scanf("%s",&n);
+    scanf("%s%s%s",&n1,&n2,&n3);
     printf("\n\tEnter Student Reg.no \t:");
     scanf("%s",&r);
-    printf("\n\tEnter Student e-mail id \t:");
-    scanf("%s",&e);
+    printf("\n\tEnter Student e-mail id :");
+    scanf("%s",email);
     printf("\n\tEnter Student Phone.no \t:");
     scanf("%s",&p);
 
-    printf("\n\n\t\t\tStudent Details:");
+    printf("\n\n\t\t\tStudent Details:\n");
     printf("\t\t\t---------------");
 
-    printf("\n\tStudent Name\t:%s",n);
-    printf("\n\tStudent Reg.no\t:%s",r);
-    printf("\n\tStudent e-mail id\t:%s",e);
-    printf("\n\tStudent Phone.no\t:%s",p);
+    printf("\n\tStudent Name\t : %s %s %s",n1,n2,n3);
+    printf("\n\tStudent Reg.no\t : %s",r);
+    printf("\n\tStudent e-mail id: %s",email);
+    printf("\n\tStudent Phone.no : %s",p);
 
     printf("\n\n\nSuccessfully Executed...\n\n");
     return 0;
